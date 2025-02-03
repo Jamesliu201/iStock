c8ae6f03d113642ee251cd718ab965c60c82dd57
j58liu <j58liu@uwaterloo.ca>
2025-02-03 10:04:30 -0500

    Program setup, realtime data function set

--- data_fetch.py
+++ data_fetch.py
@@ -1,1 +1,15 @@
+import os
+import requests
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
+
+
+def get_realtime_data(stock_symbol):
+
+    api_key = os.getenv('JQ2IPQUPV4M9QIIB')
+
+    if not api_key:
+        raise ValueError("No api key bruh")
